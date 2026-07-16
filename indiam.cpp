57d458d34b99ea7ec6f06d6f3808e3144a913b98
Gaurav-make991 <gauravsingh24072005@gmail.com>
2026-07-16 12:47:05 +0530

    source code of module 1

---
+++ indiam.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the size of the Inverted Diamond: ";
+    cin >> n;
+  
+    //lower half diamond
+    for(int i = n - 1; i >= 0; i--){
+        for(int j = 1; j <= n - i; j++){
+            cout << "  ";
+        }
+       
+    
+    for(int j = 1; j <= i + 1; j++){
+            cout << "* ";
+        }
+       
+
+       
+        for(int j = 1; j <= i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
